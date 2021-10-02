703cd221f9fbd6ee2f6889ffd95bd30218b30580
Nihal <58914812+nihalr13@users.noreply.github.com>
2021-10-01 22:49:18 -0400

    Basic Create/Delete Template
    Javascript code for basic page with create and delete buttons, buttons are not functional yet

---
+++ Project/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from "react";
+import Button from "./Button.js";
+
+class App extends Component {
+  state = {
+    name: "king"
+  };
+  changeName = () => {
+    this.setState({
+      name: "queen"
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Ollie</h1>
+        <Button name="Create Story" click={this.changeName} />
+        <Button name="Delete Story" click={this.changeName} />
+      </div>
+    );
+  }
+}
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+export default App;
