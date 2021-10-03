492ca4434182a4570c03bf97c737436640068407
Nihal <58914812+nihalr13@users.noreply.github.com>
2021-10-03 12:44:08 -0400

    Textboxes
    Added textboxes to create story UI

--- Project/CreateStory.js
+++ Project/CreateStory.js
@@ -3,7 +3,16 @@ import React from "react";
 function CreateStory() {
     return (
         <div>
-            <p>Enter your story info here.</p>
+            <h1><p>Enter your story info on this page.</p></h1>
+            <form>
+                <label>
+                    Name of Story: <input type="text" />
+                </label>
+                <br></br>
+                <label>
+                    Description of Story: <input type="text" />
+                </label>
+            </form>
         </div>
     );
 }
