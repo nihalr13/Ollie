350bf0cca035ae4e612cd279e8e6f3e2daf2d7e1
Shashank Kumar <54077000+kumar406@users.noreply.github.com>
2021-10-03 22:01:16 -0400

    Created template for script

---
+++ script.js
@@ -0,0 +1,39 @@
+let filters = document.querySelectorAll(".filter div");
+let grid = document.querySelector(".grid");
+let addBtn = document.querySelector(".add");
+let delBtn = document.querySelector(".action2");
+
+delBtn.addEventListener("click", function(e) {
+
+});
+addBtn.addEventListener("click", function(e) {
+ 
+});
+
+
+function LoadTasks(PassedColor, removeState) {
+
+}
+
+function WritingAreaTicketHandler(e) {
+
+}
+
+function TicketCreator(e) {
+
+}
+
+function DeleteFunc(e) {
+  
+}
+
+function LoadDeletedTasks(PassedColor, removeState) {
+
+}
+
+function lightmode() {
+
+}
+function DarkMode() {
+
+}
