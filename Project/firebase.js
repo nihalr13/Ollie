aa7b6ba422d048ddebdfbd9b91be75d78d7a6bf9
Dion Peng <pengdion@gmail.com>
2021-10-10 18:21:59 -0400

    [WIP] React Log-in, Log-in with Google Account

--- Project/firebase.js
+++ Project/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import * as firebase from 'firebase';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,5 +18,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+class Firebase {
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+  }
+}
+  
+export default Firebase;
