e94992fb4997db0e03f4dcf8ba4213cda121a1b8
Nihal <58914812+nihalr13@users.noreply.github.com>
2021-10-10 21:40:38 -0400

    Priority and Time Estimate
    Added priority and time estimate to createStory UI, will change priority to dropdown

--- Project/CreateStory.js
+++ Project/CreateStory.js
@@ -3,9 +3,16 @@ import logo from './logo.svg';
 import './CreateStory.css';
 import {Link } from "react-router-dom";
 //export { CreateStory };
+export { val };
 
+var val;
 export function Hello() {
     return (<div>hello</div>);
+}
+
+export function save() {
+    val = document.getElementById("storydsc").value;
+    document.write(val);
 }
 
 //var dsc = document.getElementById('storydsc').value;
@@ -16,6 +23,8 @@ function CreateStory() {
     const [state, setState] = useState({
         storyName: "",
         storyDesc: "",
+        priority: "",
+        timeEstimate: ""
     })
 
 
@@ -55,13 +64,29 @@ function CreateStory() {
                         onChange={handleChange}
                     />
                 </label>
+                <br></br>
+                <label>
+                    Priority:{" "} 
+                    <input 
+                    type="text" 
+                    name="priority"
+                    value={state.priority} 
+                    onChange={handleChange}
+                    />
+                </label>
+                <br></br>
+                <label>
+                    Time Estimate:{" "} 
+                    <input 
+                        type="text" 
+                        name="timeEstimate"
+                        value={state.timeEstimate} 
+                        onChange={handleChange}
+                    />
+                </label>
             </form>
-            <body>
-              <input type="text" name="enter" class="enter" value="" id="storydsc"/>
-              <input type="button" value="click" OnClick="save();"/>
-            </body>
             <Link to="/Board"><button>
-          Next
+            Next
           </button> 
           </Link>
         </div>
