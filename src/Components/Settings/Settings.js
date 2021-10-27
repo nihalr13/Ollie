5dca23b91ad67f5d23ca9aec606031173f553d05
Dion Peng <pengdion@gmail.com>
2021-10-27 10:27:29 -0400

    refactor settings

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -2,65 +2,69 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import './settings.css'
 import { getAuth, deleteUser } from "firebase/auth";
+import app from "../../initialize";
+import { getDatabase, ref, update } from "firebase/database";
 
+const db = getDatabase(app);
 
 //card and card-body classNamees taken from bootstrap
 //modal text inspired by https://www.w3schools.com/howto/howto_css_modals.asp
 
-function deleteAcc() {
-  const auth = getAuth();
-  const user = auth.currentUser;
-  if (window.confirm("Are you sure you want to delete this account?")) {
-    deleteUser(user).then(() => {
-      // User deleted, so sign them out and navigate to main page.
-      this.context.router.push("/login");
-    }).catch((error) => {
-      // An error ocurred
-      // ...
-    });
-  } else {
-    // remove popup and do nothing
+class Settings extends Component {
+
+  DeleteAcc() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (window.confirm("Are you sure you want to delete this account?")) {
+      deleteUser(user).then(() => {
+        alert("User deleted successfully.")
+        // User deleted, so sign them out and navigate to log-in page.
+        this.props.history.push("/login");
+      }).catch((error) => {
+        // An error ocurred
+        // ...
+      });
+    } else {
+      // remove popup and do nothing
+    }
   }
-}
 
-function Settings() {
+    render() { return <div>
+      <body>
+      <div class="settings-header"></div>
 
-    return <div>
-    <body>
-    <div class="settings-header"></div>
+      <div class="settings-container">
+          <div class="settings-item">
+              <label>default story category</label>
+              <select id="story_category">
+                  <option value="backlog">backlog</option>
+                  <option value="in progress">in progress</option>
+                  <option value="blocked">blocked</option>
+                  <option value="done">done</option>
+              </select>
+          </div>
+          <div class="settings-item">
+              <label>broswer notifications</label>
+              <select id="browser_notifications">
+                  <option value="enable">enable</option>
+                  <option value="disable">disable</option>
+              </select>
+          </div>
+          <div class="settings-item">
+              <label>email notifications</label>
+              <select id="email_notifications">
+                  <option value="enable">enable</option>
+                  <option value="disable">disable</option>
+              </select>
+          </div>
+          <button type="button" onClick={ this.DeleteAcc } >Delete Account</button>
+          <button type="button" onclick="onSave()">Save</button>
 
-    <div class="settings-container">
-        <div class="settings-item">
-            <label>default story category</label>
-            <select id="story_category">
-                <option value="backlog">backlog</option>
-                <option value="in progress">in progress</option>
-                <option value="blocked">blocked</option>
-                <option value="done">done</option>
-            </select>
-        </div>
-        <div class="settings-item">
-            <label>broswer notifications</label>
-            <select id="browser_notifications">
-                <option value="enable">enable</option>
-                <option value="disable">disable</option>
-            </select>
-        </div>
-        <div class="settings-item">
-            <label>email notifications</label>
-            <select id="email_notifications">
-                <option value="enable">enable</option>
-                <option value="disable">disable</option>
-            </select>
-        </div>
-        <button type="button" onclick="deleteAcc()">Delete Account</button>
-        <button type="button" onclick="onSave()">Save</button>
-
-        
-    </div>
-    </body>
-    </div>
-    
+          
+      </div>
+      </body>
+      </div>
+    }
 }
 
 
