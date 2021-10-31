a6596eadc303ff5fe49bd10afe46590502e7e7af
Qusai Ghabrah <qghabrah@purdue.edu>
2021-10-31 05:21:03 -0400

    Fixed text overflowing the div

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -60,8 +60,6 @@ function Board() {
         storiesAsObj.push(new Story(childNames[i], childDescriptions[i], childTimes[i], assigner_, assignee_, childPriorities[i], childCategories[i], childDates[i]));
       }
 
-      console.log(storiesAsObj);
-
       setStories(storiesAsObj);
     });
 
@@ -155,7 +153,7 @@ function MyVerticallyCenteredModal(props) {
         time = props.story.time;
         priority = props.story.priority;
         category = props.story.category;
-        date = props.story.date_created;
+        date = props.story.dateCreated;
         //alert(props.story.date_created);
     }
     var priorityColor = "";
