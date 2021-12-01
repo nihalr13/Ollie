e7c7c807dc486e887bd48dccec2da1b13b1daa8b
Dion Peng <pengdion@gmail.com>
2021-12-01 16:13:59 -0500

    GET /user

--- src/login.component.js
+++ src/login.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { getAuth, signInWithEmailAndPassword, signInWithPopup, GithubAuthProvider  } from "firebase/auth";
 import {Link, useHistory } from "react-router-dom";
+const { Octokit } = require("@octokit/core");
+
 
 const provider = new GithubAuthProvider();
 
@@ -53,20 +55,10 @@ export default class Login extends Component {
         // This gives you a GitHub Access Token. You can use it to access the GitHub API.
         const credential = GithubAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
-        const request = new XMLHttpRequest();
-        //Enter username after users/ to see list of repos
-        const url = "https://api.github.com/users/username/repos";
-        request.open("GET", url, true);
-        request.onload = function() {
-          alert(this.response);
-        }
-        request.send();
-        // The signed-in user info.
-        const user = result.user;
-        //const history = useHistory();
-        //history.push("/Board");
-        this.props.history.push("/Board");
-
+        const octokit = new Octokit({ auth: token });
+        octokit.request('GET /user').then(response => {
+          const user = response.data.name;
+        });
         // ...
       }).catch((error) => {
         // Handle Errors here.
