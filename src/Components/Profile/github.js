1ae9db8f669a3f67f867d30597e0e7a18f5635fa
Dion Peng <pengdion@gmail.com>
2021-12-01 18:33:53 -0500

    dotenv secrets token

--- src/Components/Profile/github.js
+++ src/Components/Profile/github.js
@@ -1,7 +1,8 @@
 const { Octokit } = require("@octokit/core");
-
+require('dotenv').config();
+const token = process.env.token;
 const poll = (owner, repo) => {
-  const octokit = new Octokit({ auth: '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' });
+  const octokit = new Octokit({ auth: token});
   octokit.request('GET /repos/{owner}/{repo}/commits', {
     owner: owner,
     repo: repo
