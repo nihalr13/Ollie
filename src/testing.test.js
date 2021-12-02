fe66707258dc32b1cce3aefbd7ec917987f65497
Qusai Ghabrah <qghabrah@purdue.edu>
2021-12-01 22:26:55 -0500

    test backfrop remove

--- src/testing.test.js
+++ src/testing.test.js
@@ -4,6 +4,6 @@ import { toBeInTheDocument } from '@testing-library/jest-dom'
 import App from './App';
 
 test('render h1 element', () => {
-  render(<App />);
+  render(<BrowserRouter><App /></BrowserRouter>);
   expect(screen.getByText('Ollie')).toBeInTheDocument();
 });
