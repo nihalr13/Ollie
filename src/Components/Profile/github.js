1c63616618ac7a5e8de7fce3393a1014280a6d58
nihalr13 <nihal.rajan@yahoo.com>
2021-12-03 16:00:13 -0500

    Additional Scripting Comment

--- src/Components/Profile/github.js
+++ src/Components/Profile/github.js
@@ -13,6 +13,7 @@ const poll = (owner, repo) => {
     if ((localStorage.oldCommit).localeCompare("0") == 0) {
       localStorage.oldCommit = response.data[0].sha;
     } else {
+      //Get new commit
       var newCommit = response.data[0].sha;
       if (newCommit.localeCompare(localStorage.oldCommit) != 0) {
         //Set old commit to new commit
