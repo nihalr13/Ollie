310f9c6e7f54f85cd94e3c380d350f9b13e729be
kumar406 <shashankkumar453@gmail.com>
2021-12-02 21:00:14 -0500

    added user emails to profile page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -25,37 +25,26 @@ function signout() {
   }
 }
 
-const auth = getAuth();
-const user = auth.currentUser;
-if (user !== null) {
-  // The user object has basic properties such as display name, email, etc.
-  const displayName = user.displayName;
-  const email = user.email;
-  const photoURL = user.photoURL;
-  const emailVerified = user.emailVerified;
-  console.log(user.email);
+function Profile () {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    var user_email;
+    if (user !== null) {
+        user_email = user.email;
+        console.log(user.email);
+    } else {
+        console.log("user is null");
+    }
 
-  // The user's ID, unique to the Firebase project. Do NOT use
-  // this value to authenticate with your backend server, if
-  // you have one. Use User.getToken() instead.
-  const uid = user.uid;
-} else {
-  console.log("user is null");
-}
-
-const currUser = {
-    name: "Qusai",
-    email: "Ollie",
-    repoOwner: "QAGatPurdue",
-    repoName: "Qusai@gmail.com",
-    profileImg: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2016%2F11%2Frock-insta.jpg"
-}
-
-const Profile = () => {
+    const currUser = {
+        email: user_email,
+        repoOwner: "QAGatPurdue",
+        repoName: "Ollie",
+        profileImg: "https://freesvg.org/img/abstract-user-flat-4.png"  
+    }
 
     const initialState = {
         isEdit: false,
-        name: currUser.name,
         email: currUser.email,
         repoOwner: currUser.repoOwner
     }
@@ -65,20 +54,17 @@ const Profile = () => {
     const handleEdit = () => {
         setState({
             isEdit: true,
-            name: state.name,
             email: state.email,
             repoOwner: state.repoOwner
         })
     }
 
     const handleSave = (event) => {
-        currUser.name = state.name;
         currUser.email = state.email;
         currUser.repoOwner = state.repoOwner;
 
         setState({
             isEdit: false,
-            name: state.name,
             email: state.email,
             repoOwner: state.repoOwner
         })
@@ -86,19 +72,9 @@ const Profile = () => {
         
     }
 
-    const handleChangeName = (event) => {
-        setState({
-            isEdit: state.isEdit,
-            name: event.target.value,
-            email: state.email,
-            repoOwner: state.repoOwner
-        })
-    }
-
     const handleChangeEmail = (event) => {
         setState({
             isEdit: state.isEdit,
-            name: state.name,
             email: event.target.value,
             repoOwner: state.repoOwner
         })
@@ -107,27 +83,23 @@ const Profile = () => {
     const handleChangerepoOwner = (event) => {
         setState({
             isEdit: state.isEdit,
-            name: state.name,
             email: state.email,
             repoOwner: event.target.value
         })
     }
 
-    var fullNameComp;
     var EmailComp;
     var GitComp;
     var repoNameComp;
     var button;
 
     if (state.isEdit) {
-        fullNameComp = <Col md="auto" lg="auto"><input type="text" onChange={handleChangeName} value={state.name}></input></Col>;
         EmailComp = <Col md="auto" lg="auto"><input type="text" onChange={handleChangeEmail} value={state.email}></input></Col>;
         GitComp = <Col md="auto" lg="auto"><input type="text" onChange={handleChangerepoOwner} value={state.repoOwner}></input></Col>;;
         repoNameComp = <Col md="auto" lg="auto">{currUser.repoName}</Col>;
         button = <button id="edit-btn" onClick={handleSave}>Save Changes</button>;
     }
     else {
-        fullNameComp = <Col md="auto" lg="auto">{currUser.name}</Col>;
         EmailComp = <Col md="auto" lg="auto">{currUser.email}</Col>;
         GitComp = <Col md="auto" lg="auto">{currUser.repoOwner}</Col>;
         repoNameComp = <Col md="auto" lg="auto">{currUser.repoName}</Col>;
@@ -141,28 +113,16 @@ const Profile = () => {
             <div className="sidebar">
                 <Sidebar />
             </div>
-
-            
-
             <div className="page-content">
                 <div>
-                {/* TODO: Implement changing image */}
                 <img id="profile-img" src={currUser.profileImg} />
                 </div>
                 <div id="profile-info">
                     <div>
                         <Container>  
                             <Row className="center">
-                                <Col>Display Name: </Col>
-                                {fullNameComp}
-                            </Row>
-                        </Container>
-                    </div>
-                    <div>
-                        <Container>  
-                            <Row className="center">
                                 <Col>Repository Name: </Col>
-                                {EmailComp}
+                                {repoNameComp}
                             </Row>
                         </Container>
                     </div>
@@ -178,7 +138,7 @@ const Profile = () => {
                         <Container>  
                             <Row className="center">
                                 <Col>Email: </Col>
-                                {repoNameComp}
+                                {EmailComp}
                             </Row>
                         </Container>
                     </div>
